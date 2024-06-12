99e1992b96c91557e747cb3d5d5f1627117c7eda
Jasczlv <ianis.cozlov@gmail.com>
2024-06-12 18:40:10 +0200

    Adding API

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    //
+    public function index()
+    {
+        //
+        $results = Project::with('type', 'technologies')->paginate(10);
+        return response()->json([
+            'success' => true,
+            'results' => $results
+        ]);
+    }
+}
